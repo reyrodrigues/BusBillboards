a815acbc7f45f81a8e6335245cb5d6c46a20f0d8
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2015-12-08 18:37:39 -0500

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        copy: {
+            main: {
+                files: [
+                    {expand: true, cwd: 'bower_components/angular/', src: ['angular.js'], dest: 'js/'},
+                    {expand: true, cwd: 'bower_components/angular-ckeditor/', src: ['angular-ckeditor.js'], dest: 'js/'},
+                    {expand: true, cwd: 'bower_components/angular-ui-router/release/', src: ['angular-ui-router.js'], dest: 'js/'},
+                    {expand: true, cwd: 'bower_components/moment/min/', src: ['moment-with-locales.js'], dest: 'js/'},
+                    {expand: true, cwd: 'bower_components/ckeditor/', src: ['**'], dest: 'js/ckeditor/'},
+                ]
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Default task(s).
+    grunt.registerTask('default', ['copy:main']);
+
+};
