1a5b68731ac8e9f55a2e6cca2c34089239de7c6a
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2015-12-17 12:29:04 -0500

    Buildpacks:

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,23 +6,28 @@ module.exports = function (grunt) {
         copy: {
             main: {
                 files: [
-                    {expand: true, cwd: 'bower_components/jquery/dist/', src: ['jquery.js'], dest: 'board/static/js/'},
-                    {expand: true, cwd: 'bower_components/jquery-ui/', src: ['jquery-ui.js'], dest: 'board/static/js'},
-                    {expand: true, cwd: 'bower_components/angular-ui-sortable/', src: ['sortable.js'], dest: 'board/static/js'},
-                    {expand: true, cwd: 'bower_components/angular/', src: ['angular.js'], dest: 'board/static/js/'},
-                    {expand: true, cwd: 'bower_components/angular-ckeditor/', src: ['angular-ckeditor.js'], dest: 'board/static/js/'},
-                    {expand: true, cwd: 'bower_components/angular-ui-router/release/', src: ['angular-ui-router.js'], dest: 'board/static/js/'},
-                    {expand: true, cwd: 'bower_components/moment/min/', src: ['moment-with-locales.js'], dest: 'board/static/js/'},
-                    {expand: true, cwd: 'bower_components/ckeditor/', src: ['**'], dest: 'board/static/js/ckeditor/'},
+                    {expand: true, cwd: 'lib/jquery/dist/', src: ['jquery.js'], dest: 'board/static/js/'},
+                    {expand: true, cwd: 'lib/jquery-ui/', src: ['jquery-ui.js'], dest: 'board/static/js'},
+                    {expand: true, cwd: 'lib/angular-ui-sortable/', src: ['sortable.js'], dest: 'board/static/js'},
+                    {expand: true, cwd: 'lib/angular/', src: ['angular.js'], dest: 'board/static/js/'},
+                    {expand: true, cwd: 'lib/angular-ckeditor/', src: ['angular-ckeditor.js'], dest: 'board/static/js/'},
+                    {expand: true, cwd: 'lib/angular-ui-router/release/', src: ['angular-ui-router.js'], dest: 'board/static/js/'},
+                    {expand: true, cwd: 'lib/moment/min/', src: ['moment-with-locales.js'], dest: 'board/static/js/'},
+                    {expand: true, cwd: 'lib/ckeditor/', src: ['**'], dest: 'board/static/js/ckeditor/'},
                 ]
+            }
+        },
+        bower: {
+            install: {
             }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-bower-task');
 
     // Default task(s).
-    grunt.registerTask('dokku:production', ['copy:main']);
+    grunt.registerTask('dokku:production', ['bower:install', 'copy:main']);
 
 };
